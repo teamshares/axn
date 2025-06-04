f0faa8d813bb8455bebf8acf43ba8dace85e6841
Kali Donovan <kali@teamshares.com>
2025-06-04 13:47:15 -0700

    Callback: error + failure (and refactor internals)

--- lib/action/core/swallow_exceptions.rb
+++ lib/action/core/swallow_exceptions.rb
@@ -1,42 +1,16 @@
 # frozen_string_literal: true
 
+require_relative "event_handlers"
+
 module Action
   module SwallowExceptions
-    CustomErrorInterceptor = Data.define(:matcher, :message, :should_report_error)
-    CustomErrorHandler = Data.define(:matcher, :block)
-
-    class CustomErrorInterceptor
-      def self.matches?(matcher:, exception:, action:)
-        if matcher.respond_to?(:call)
-          if matcher.arity == 1
-            !!action.instance_exec(exception, &matcher)
-          else
-            !!action.instance_exec(&matcher)
-          end
-        elsif matcher.is_a?(String) || matcher.is_a?(Symbol)
-          klass = Object.const_get(matcher.to_s)
-          klass && exception.is_a?(klass)
-        elsif matcher < Exception
-          exception.is_a?(matcher)
-        else
-          action.warn("Ignoring apparently-invalid matcher #{matcher.inspect} -- could not find way to apply it")
-          false
-        end
-      rescue StandardError => e
-        action.warn("Ignoring #{e.class.name} raised while determining matcher: #{e.message}")
-        false
-      end
-
-      def matches?(exception:, action:)
-        self.class.matches?(matcher:, exception:, action:)
-      end
-    end
-
     def self.included(base)
       base.class_eval do
         class_attribute :_success_msg, :_error_msg
         class_attribute :_custom_error_interceptors, default: []
+        class_attribute :_error_handlers, default: []
         class_attribute :_exception_handlers, default: []
+        class_attribute :_failure_handlers, default: []
 
         include InstanceMethods
         extend ClassMethods
@@ -44,9 +18,22 @@ module Action
         def run_with_exception_swallowing!
           original_run!
         rescue StandardError => e
-          raise if e.is_a?(Action::Failure) # TODO: avoid raising if this was passed along from a child action (esp. if wrapped in hoist_errors)
+          # on_error handlers run for both unhandled exceptions and fail!
+          self.class._error_handlers.each do |handler|
+            handler.execute_if_matches(exception: e, action: self)
+          end
 
-          # Add custom hook for intercepting exceptions (e.g. Teamshares automatically logs to Honeybadger)
+          # on_failure handlers run ONLY for fail!
+          if e.is_a?(Action::Failure)
+            self.class._failure_handlers.each do |handler|
+              handler.execute_if_matches(exception: e, action: self)
+            end
+
+            # TODO: avoid raising if this was passed along from a child action (esp. if wrapped in hoist_errors)
+            raise e
+          end
+
+          # on_exception handlers run for ONLY for unhandled exceptions. AND NOTE: may be skipped if the exception is rescued via `rescues`.
           trigger_on_exception(e)
 
           @context.exception = e
@@ -65,15 +52,17 @@ module Action
           raise if @context.object_id != e.context.object_id
         end
 
-        def trigger_on_exception(e)
-          interceptor = self.class._error_interceptor_for(exception: e, action: self)
+        def trigger_on_exception(exception)
+          interceptor = self.class._error_interceptor_for(exception:, action: self)
           return if interceptor&.should_report_error == false
 
           # Call any handlers registered on *this specific action* class
-          _on_exception(e)
+          self.class._exception_handlers.each do |handler|
+            handler.execute_if_matches(exception:, action: self)
+          end
 
           # Call any global handlers
-          Action.config.on_exception(e,
+          Action.config.on_exception(exception,
                                      action: self,
                                      context: respond_to?(:context_for_logging) ? context_for_logging : @context.to_h)
         rescue StandardError => e
@@ -114,10 +103,25 @@ module Action
         _register_error_interceptor(matcher, message, should_report_error: false, **match_and_messages)
       end
 
-      def on_exception(matcher = StandardError, &block)
+      # ONLY raised exceptions (i.e. NOT fail!). Skipped if exception is rescued via .rescues.
+      def on_exception(matcher = StandardError, &handler)
         raise ArgumentError, "on_exception must be called with a block" unless block_given?
 
-        self._exception_handlers += [CustomErrorHandler.new(matcher:, block:)]
+        self._exception_handlers += [Action::EventHandlers::ConditionalHandler.new(matcher:, handler:)]
+      end
+
+      # ONLY raised on fail! (i.e. NOT unhandled exceptions).
+      def on_failure(matcher = Action::Failure, &handler)
+        raise ArgumentError, "on_failure must be called with a block" unless block_given?
+
+        self._failure_handlers += [Action::EventHandlers::ConditionalHandler.new(matcher:, handler:)]
+      end
+
+      # Handles both fail! and unhandled exceptions... but is NOT affected by .rescues
+      def on_error(matcher = StandardError, &handler)
+        raise ArgumentError, "on_error must be called with a block" unless block_given?
+
+        self._error_handlers += [Action::EventHandlers::ConditionalHandler.new(matcher:, handler:)]
       end
 
       # Syntactic sugar for "after { try" (after, but if it fails do NOT fail the action)
@@ -143,9 +147,11 @@ module Action
         method_name = should_report_error ? "error_from" : "rescues"
         raise ArgumentError, "#{method_name} must be called with a key/value pair, or else keyword args" if [matcher, message].compact.size == 1
 
-        { matcher => message }.compact.merge(match_and_messages).each do |(matcher, message)| # rubocop:disable Lint/ShadowingOuterLocalVariable
-          self._custom_error_interceptors += [CustomErrorInterceptor.new(matcher:, message:, should_report_error:)]
+        interceptors = { matcher => message }.compact.merge(match_and_messages).map do |(matcher, message)| # rubocop:disable Lint/ShadowingOuterLocalVariable
+          Action::EventHandlers::CustomErrorInterceptor.new(matcher:, message:, should_report_error:)
         end
+
+        self._custom_error_interceptors += interceptors
       end
     end
 
@@ -170,18 +176,6 @@ module Action
       end
 
       delegate :default_error, to: :internal_context
-
-      def _on_exception(exception)
-        handlers = self.class._exception_handlers.select do |this|
-          CustomErrorInterceptor.matches?(matcher: this.matcher, exception:, action: self)
-        end
-
-        handlers.each do |handler|
-          instance_exec(exception, &handler.block)
-        rescue StandardError => e
-          warn("Ignoring #{e.class.name} in on_exception hook: #{e.message}")
-        end
-      end
     end
   end
 end
--- spec/action/core/hooks_and_callbacks_spec.rb
+++ spec/action/core/hooks_and_callbacks_spec.rb
@@ -2,21 +2,22 @@
 
 RSpec.describe Action do
   describe "Hooks & Callbacks" do
-    subject(:result) { action.call(should_raise:, should_after_raise:) }
+    subject(:result) { action.call(should_fail:, should_raise:, should_after_raise:) }
 
     let(:action) do
       Class.new do
         include Action
+        expects :should_fail, type: :boolean
         expects :should_raise, type: :boolean
         expects :should_after_raise, type: :boolean
 
-        before do
-          puts "before"
-        end
+        before { puts "before" }
 
-        on_success do
-          puts "on_success"
-        end
+        # Callbacks
+        on_success { puts "on_success" }
+        on_failure { puts "on_failure" }
+        on_error { puts "on_error" }
+        on_exception { puts "on_exception" }
 
         after do
           puts "after"
@@ -26,10 +27,13 @@ RSpec.describe Action do
         def call
           puts "calling"
           raise "bad" if should_raise
+
+          fail!("Custom failure message") if should_fail
         end
       end
     end
 
+    let(:should_fail) { false }
     let(:should_raise) { false }
     let(:should_after_raise) { false }
 
@@ -41,23 +45,33 @@ RSpec.describe Action do
       end
     end
 
-    context "when not ok?" do
+    context "when exception raised" do
       let(:should_raise) { true }
 
       it "does not execute on_success" do
         expect do
           expect(result).not_to be_ok
-        end.to output("before\ncalling\n").to_stdout
+        end.to output("before\ncalling\non_error\non_exception\n").to_stdout
       end
     end
 
-    context "when after hook fails" do
+    context "when exception raised in after hook" do
       let(:should_after_raise) { true }
 
       it "does not execute on_success" do
         expect do
           expect(result).not_to be_ok
-        end.to output("before\ncalling\nafter\n").to_stdout
+        end.to output("before\ncalling\nafter\non_error\non_exception\n").to_stdout
+      end
+    end
+
+    context "when fail! is called" do
+      let(:should_fail) { true }
+
+      it "executes on_failure" do
+        expect do
+          expect(result).not_to be_ok
+        end.to output("before\ncalling\non_error\non_failure\n").to_stdout
       end
     end
   end
