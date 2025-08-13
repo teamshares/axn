88ef9867c9b6ed6039e4680f7866cf5c6e08dafb
Kali Donovan <kali@teamshares.com>
2025-08-13 16:32:43 -0700

    [FEAT] add support for if: :some_method

--- lib/action/core/event_handlers.rb
+++ lib/action/core/event_handlers.rb
@@ -57,7 +57,7 @@ module Action
       def matches?(exception:, action:)
         return true if @matcher.nil?
 
-        @matcher.matches?(exception:, action:)
+        @matcher.call(exception:, action:)
       end
 
       def execute_if_matches(action:, exception:)
@@ -82,7 +82,7 @@ module Action
       def matches?(exception:, action:)
         return true if static?
 
-        @matcher.matches?(exception:, action:)
+        @matcher.call(exception:, action:)
       end
 
       # Returns a string (truthy) when it applies and yields a non-blank message; otherwise nil
@@ -99,31 +99,70 @@ module Action
     end
 
     class Matcher
-      def initialize(matcher)
-        @matcher = matcher
+      def initialize(rule)
+        @rule = rule
       end
 
-      def matches?(exception:, action:)
-        if matcher.respond_to?(:call)
-          if matcher.arity == 1
-            !!action.instance_exec(exception, &matcher)
-          else
-            !!action.instance_exec(&matcher)
-          end
-        elsif matcher.is_a?(String) || matcher.is_a?(Symbol)
-          klass = Object.const_get(matcher.to_s)
-          klass && exception.is_a?(klass)
-        elsif matcher < Exception
-          exception.is_a?(matcher)
-        else
-          action.warn("Ignoring apparently-invalid matcher #{matcher.inspect} -- could not find way to apply it")
-          false
-        end
+      def call(exception:, action:)
+        return apply_callable(action:, exception:) if callable?
+        return apply_symbol(action:, exception:) if symbol?
+        return apply_string(exception:) if string?
+        return apply_exception_class(exception:) if exception_class?
+
+        handle_invalid(action:)
       rescue StandardError => e
         Axn::Util.piping_error("determining if handler applies to exception", action:, exception: e)
       end
 
-      private attr_reader :matcher
+      private
+
+      def callable? = @rule.respond_to?(:call)
+      def symbol? = @rule.is_a?(Symbol)
+      def string? = @rule.is_a?(String)
+      def exception_class? = @rule.is_a?(Class) && @rule <= Exception
+
+      def apply_callable(action:, exception:)
+        if @rule.arity == 1
+          !!action.instance_exec(exception, &@rule)
+        else
+          !!action.instance_exec(&@rule)
+        end
+      end
+
+      def apply_symbol(action:, exception:)
+        if action.respond_to?(@rule)
+          method = action.method(@rule)
+          if method_accepts_exception?(method)
+            !!action.public_send(@rule, exception)
+          else
+            !!action.public_send(@rule)
+          end
+        else
+          begin
+            klass = Object.const_get(@rule.to_s)
+            klass && exception.is_a?(klass)
+          rescue NameError
+            action.warn("Ignoring apparently-invalid matcher #{@rule.inspect} -- neither action method nor constant found")
+            false
+          end
+        end
+      end
+
+      def apply_string(exception:)
+        klass = Object.const_get(@rule.to_s)
+        klass && exception.is_a?(klass)
+      end
+
+      def apply_exception_class(exception:)
+        exception.is_a?(@rule)
+      end
+
+      def handle_invalid(action:)
+        action.warn("Ignoring apparently-invalid matcher #{@rule.inspect} -- could not find way to apply it")
+        false
+      end
+
+      def method_accepts_exception?(method) = method.arity == 1 || method.arity < 0
     end
   end
 end
