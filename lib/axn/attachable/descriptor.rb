889b3eb585c423a1fdeb5a9c0d14b797be763e8b
Kali Donovan <kali@teamshares.com>
2025-09-30 09:15:16 -0700

    Update descriptor.rb

--- lib/axn/attachable/descriptor.rb
+++ lib/axn/attachable/descriptor.rb
@@ -2,6 +2,14 @@
 
 module Axn
   module Attachable
-    Descriptor = Data.define(:as, :name, :axn_klass, :kwargs, :block)
+    Descriptor = Data.define(:as, :name, :axn_klass, :kwargs, :block) do
+      def attachment_type
+        AttachmentTypes.find(as)
+      end
+
+      def validate!
+        Validator.validate!(self)
+      end
+    end
   end
 end
