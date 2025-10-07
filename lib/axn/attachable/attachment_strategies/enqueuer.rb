a0ebe46ce5cf8a4ada3beea5211f33de1f32d851
Kali Donovan <kali@teamshares.com>
2025-10-07 13:12:30 -0700

    WIP - enqueuer

---
+++ lib/axn/attachable/attachment_strategies/enqueuer.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Axn
+  module Attachable
+    class AttachmentStrategies
+      module Enqueuer
+        extend Base
+
+        module DSL
+          def _enqueue_via(name = "enqueue_all", axn_klass = nil, **, &)
+            attach_axn(as: :enqueuer, name:, axn_klass:, **, &)
+          end
+        end
+
+        def self.mount(descriptor:, target:)
+          name = descriptor.name
+
+          mount_method(target:, method_name: name) do |**kwargs|
+            axn = descriptor.attached_axn_for(target: self)
+            axn.call(**kwargs)
+          end
+
+          mount_method(target:, method_name: "#{name}_async") do |**kwargs|
+            axn = descriptor.attached_axn_for(target: self)
+            axn.call_async(**kwargs)
+          end
+        end
+      end
+    end
+  end
+end
