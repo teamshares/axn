9e8c29ca83008b67300d55d5796d5dc3e1a258a2
Kali Donovan <kali@teamshares.com>
2025-10-07 16:30:09 -0700

    Update _base.rb

--- lib/axn/mountable/mounting_strategies/_base.rb
+++ lib/axn/mountable/mounting_strategies/_base.rb
@@ -21,6 +21,22 @@ module Axn
         # Mount methods directly to the target class
         def mount_to_target(descriptor:, target:) = raise NotImplementedError, "Strategy modules must implement mount_to_target"
 
+        def key = name.split("::").last.underscore.to_sym
+
+        # Helper method to define a method on target with collision checking
+        def mount_method(target:, method_name:, &)
+          # Check if method collision should raise an error
+          # We allow overriding if this is a child class with a parent that has axn methods (inheritance scenario)
+          # Otherwise, we raise an error for same-class method collisions
+          if _should_raise_method_collision_error?(target, method_name)
+            raise MountingError, "#{name.split("::").last} unable to attach -- method '#{method_name}' is already taken"
+          end
+
+          target.define_singleton_method(method_name, &)
+        end
+
+        private
+
         # Mount methods to the namespace and register the action class
         def mount_to_namespace(descriptor:, target:)
           action_class_builder = Helpers::ClassBuilder.new(descriptor)
@@ -47,22 +63,6 @@ module Axn
           # Register the action class as a constant in the namespace
           action_class_builder.mount(target, name.to_s)
         end
-
-        def key = name.split("::").last.underscore.to_sym
-
-        # Helper method to define a method on target with collision checking
-        def mount_method(target:, method_name:, &)
-          # Check if method collision should raise an error
-          # We allow overriding if this is a child class with a parent that has axn methods (inheritance scenario)
-          # Otherwise, we raise an error for same-class method collisions
-          if _should_raise_method_collision_error?(target, method_name)
-            raise MountingError, "#{name.split("::").last} unable to attach -- method '#{method_name}' is already taken"
-          end
-
-          target.define_singleton_method(method_name, &)
-        end
-
-        private
 
         # Check if we should raise an error for method collision
         # Returns true if method exists AND target is not overriding a parent's method (same-class collision)
