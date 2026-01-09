067fda67c1a17793f1f6c8f22767f1e317133749
Kali Donovan <kali@teamshares.com>
2026-01-09 14:46:12 -0800

    Simplify

--- lib/axn/mountable/helpers/class_builder.rb
+++ lib/axn/mountable/helpers/class_builder.rb
@@ -24,17 +24,12 @@ module Axn
         end
 
         def build_and_configure_action_class(target, name, namespace)
-          # Mark target as having an action class being created to prevent recursion
-          # Track the target class so we know which class is having an action created for it
-          target.instance_variable_set(:@_axn_creating_action_class_for, target)
-          begin
-            mounted_axn = build_action_class(target, _creating_action_class_for: target)
-            configure_class_name_and_constant(mounted_axn, name, namespace, target)
-            configure_axn_mounted_to(mounted_axn, target)
-            mounted_axn
-          ensure
-            target.instance_variable_set(:@_axn_creating_action_class_for, nil)
-          end
+          # Pass target through to Factory.build so it can mark the superclass
+          # The superclass flag is checked by the inherited callback in mountable.rb
+          mounted_axn = build_action_class(target, _creating_action_class_for: target)
+          configure_class_name_and_constant(mounted_axn, name, namespace, target)
+          configure_axn_mounted_to(mounted_axn, target)
+          mounted_axn
         end
 
         private
